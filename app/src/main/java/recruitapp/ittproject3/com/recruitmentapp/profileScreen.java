06d86039765ae7e966894038897b5d3778edb833
Andy <andypollard84@gmail.com>
2015-02-09 00:21:59 +0000

    Updates to user profile GUI

--- app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/profileScreen.java
+++ app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/profileScreen.java
@@ -53,13 +53,10 @@ public class profileScreen extends ActionBarActivity implements NavigationDrawer
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = "Profile";
                 break;
             case 2:
                 mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
                 break;
         }
     }
