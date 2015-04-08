3412386a35b9b5f3973b205d22df60a3a962c163
johntk <johntk86@gmail.com>
2015-04-08 11:33:31 +0100

    Test

--- app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/helper/AppConfig.java
+++ app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/helper/AppConfig.java
@@ -7,11 +7,9 @@ package recruitapp.ittproject3.com.recruitmentapp.helper;
 
 public class AppConfig {
 
-<<<<<<< HEAD
-    private static String URL = "http://192.168.0.27:9000";
-=======
+
     private static String URL = "http://192.168.1.2:9000";
->>>>>>> jtkDev1
+
 
     // Server user login url
     public static String URL_LOGIN = URL + "/androidlogin";
