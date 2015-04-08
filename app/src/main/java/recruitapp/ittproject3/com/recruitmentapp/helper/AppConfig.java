52d4e61f50985fd43316ba8d7a1726d647ab822a
johntk <johntk86@gmail.com>
2015-04-08 10:58:03 +0100

    Fixed volley

---
+++ app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/helper/AppConfig.java
@@ -0,0 +1,17 @@
+package recruitapp.ittproject3.com.recruitmentapp.helper;
+
+/**
+ * Created by Andrew on 07/04/2015.
+ */
+
+
+public class AppConfig {
+
+    private static String URL = "http://192.168.1.2:9000";
+
+    // Server user login url
+    public static String URL_LOGIN = URL + "/androidlogin";
+
+    // Server register URL
+    public static String URL_REGISTER = URL + "/androidregister";
+}
