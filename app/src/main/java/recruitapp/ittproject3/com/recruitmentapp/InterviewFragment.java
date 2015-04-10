954e386fbd8fe2a90db183f26fad93c6bb3ee957
Andy <andypollard84@gmail.com>
2015-04-10 16:46:56 +0100

    Update

--- app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/InterviewFragment.java
+++ app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/InterviewFragment.java
@@ -1,16 +1,24 @@
 package recruitapp.ittproject3.com.recruitmentapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * INTERVIEW SCREEN FRAGMENT.
  */
 public class InterviewFragment extends Fragment {
+
+    private Context mFragmentContext=getActivity().getApplicationContext();
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -37,6 +45,14 @@ public class InterviewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_interview_screen, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        for(int i =0; i<10; i++) {
+            Button myButt=new Button(mFragmentContext);
+        }
     }
 
     @Override
