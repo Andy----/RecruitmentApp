4be3c792c47293519085ad13515fd1a1cecf8a9e
Andy <andypollard84@gmail.com>
2015-04-19 23:47:41 +0100

    Almost working interview activity

--- app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/Interview.java
+++ app/src/main/java/recruitapp/ittproject3/com/recruitmentapp/Interview.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import recruitapp.ittproject3.com.recruitmentapp.Models.*;
@@ -22,8 +24,10 @@ public class Interview extends Activity {
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
     private List<InterviewQuestion> interviewQuestions;
+    private List<InterviewTaskParams> interviewTaskParamsList = new ArrayList<>();
     private SQLiteHandler db;
-
+    private Button nextQuestion;
+    private int questionCount = 0, pos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +35,34 @@ public class Interview extends Activity {
         setContentView(R.layout.activity_interview);
 
         db = new SQLiteHandler(getApplicationContext());
-
         interviewQuestions = db.getInterviewQuestionList();
+        nextQuestion = (Button) findViewById(R.id.nextQuestionButton);
 
-        // Starts the videoInterview Task
-
-
+        // Create a List of task parameter objects based on the list of interview questions
         for(int i=0; i<interviewQuestions.size(); i++) {
-            InterviewTaskParams itParams = new InterviewTaskParams((i+1), interviewQuestions.get(i).getQuestion());
-            InterviewQuestionSyncTask iqTask = new InterviewQuestionSyncTask();
-            iqTask.execute(itParams);
+            interviewTaskParamsList.add(new InterviewTaskParams((i+1), interviewQuestions.get(i).getQuestionId(), interviewQuestions.get(i).getQuestion()));
+            questionCount++;
         }
+
+
+            nextQuestion.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    if (questionCount > 0) {
+                        InterviewQuestionSyncTask iqTask = new InterviewQuestionSyncTask();
+                        iqTask.execute(interviewTaskParamsList.get(pos));
+                        pos++;
+                        questionCount--;
+                        nextQuestion.setText("Next Question");
+                    } else {
+
+                    }
+                }
+            });
     }
 
 
-    private class InterviewQuestionSyncTask extends AsyncTask<InterviewTaskParams, String, Void> {
+    private class InterviewQuestionSyncTask extends AsyncTask<InterviewTaskParams, String, Long> {
 
-        // A callback method executed on UI thread on starting the task
         @Override
         protected void onPreExecute() {
             TextView previewCounter = (TextView) findViewById(R.id.previewCounter);
@@ -58,21 +73,19 @@ public class Interview extends Activity {
         // A callback method executed on non UI thread, invoked after
         // onPreExecute method if exists
         @Override
-        protected Void doInBackground(InterviewTaskParams... params) {
+        protected Long doInBackground(InterviewTaskParams... params) {
 
-            for (int j = 5; j >= 0; j--) {
+            for (int j = 10; j >= 0; j--) {
                 try {
                     Thread.sleep(1000);
-                    publishProgress(Integer.toString(params[0].questionNumber), params[0].question, Integer.toString(j)); // Invokes onProgressUpdate()
+                    publishProgress(Integer.toString(params[0].questionNumber), params[0].question, Integer.toString(j));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            return null;
+            return params[0].questionId;
         }
 
-        // A callback method executed on UI thread, invoked by the publishProgress()
-        // from doInBackground() method
         @Override
         protected void onProgressUpdate(String... values) {
             TextView questionNumber = (TextView) findViewById(R.id.currentQuestionNumber);
@@ -85,49 +98,53 @@ public class Interview extends Activity {
 
         }
 
-        // A callback method executed on UI thread, invoked after the completion of the task
         @Override
-        protected void onPostExecute(Void result) {
-//            TextView previewCounter = (TextView) findViewById(R.id.previewCounter);
-//            previewCounter.setText("BEGIN");
+        protected void onPostExecute(Long result) {
+            TextView questionNumber = (TextView) findViewById(R.id.currentQuestionNumber);
+            TextView currentQuestion = (TextView) findViewById(R.id.currentQuestionView);
+            TextView previewCounter = (TextView) findViewById(R.id.previewCounter);
 
-            new CountDownTimer(2000, 2000) {
-                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            questionNumber.setText("");
+            currentQuestion.setText("");
+            previewCounter.setText("");
 
-                public void onTick(long millisUntilFinished) {
-//                    mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
-
-                    if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
-                        startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
-                    }
-                }
-
-                public void onFinish() {
-//                    mTextField.setText("done!");
-
-                }
-            }.start();
-
-//            dispatchTakeVideoIntent();
+            dispatchVideo(result);
+            if(questionCount == 0) {
+                nextQuestion.setText("Submit Interview");
+            }
         }
     }
 
     private static class InterviewTaskParams {
-//        List<InterviewQuestion> iqList = new ArrayList<>();
-//        InterviewTaskParams(List<InterviewQuestion> iqList) {
-//            this.iqList = iqList;
-//        }
 
         int questionNumber;
+        Long questionId;
         String question;
-        InterviewTaskParams(int questionNumber, String question){
+        InterviewTaskParams(int questionNumber, Long questionId, String question){
             this.questionNumber = questionNumber;
+            this.questionId = questionId;
             this.question = question;
         }
     }
 
-    private void dispatchTakeVideoIntent() {
+    private void dispatchVideo(Long questionId) {
+        File videoFile = null;
+        File newDir = new File(getExternalCacheDir(), "RecruitSwift/InterviewVideos");
+        if(!newDir.isDirectory())
+            newDir.mkdirs();
 
+        if(newDir.canWrite())
+            videoFile = new File(newDir, "VideoResponse"+questionId+".mp4");
+        else
+            Toast.makeText(this, "Dir not writable", Toast.LENGTH_LONG).show();
+
+
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+
+            Uri videoUri = Uri.fromFile(videoFile);
+            takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
     }
-
 }
